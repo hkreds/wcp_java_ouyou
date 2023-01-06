7bc5e0563f38d6bad080b4ae3bf530c306e9e7a4
vsckawa <109258421+hkreds@users.noreply.github.com>
2023-01-06 11:08:50 +0900

    [Finish]Chapter12 例外テスト

--- src/chapter12/CalculatorTest.java
+++ src/chapter12/CalculatorTest.java
@@ -7,13 +7,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
-
 	@Test
 	@DisplayName("2の二乗は4になる")
-	void test() {
+	void testSquare2() {
 		Calculator calc = new Calculator();
 		int result = calc.square(2);
 		assertThat(result, is(4));
 	}
-
+	@Test
+	@DisplayName("4の平方根は2になる")
+	void testRoot4() {
+		Calculator calc = new Calculator();
+		double result = calc.root(4.0);
+		assertThat(result, is(2.0));
+	}
+	@Test
+	@DisplayName("-1の平方根は例外が発生する")
+	void testRootException() {
+		Calculator calc = new Calculator();
+		assertThrows(IllegalArgumentException.class, () -> calc.root(-1));
+	}
 }
