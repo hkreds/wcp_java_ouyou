3292d3d240835e8470068c4fb6d03164049b439b
vsckawa <109258421+hkreds@users.noreply.github.com>
2023-01-06 10:48:27 +0900

    [Finish]Chapter12 テスト

---
+++ src/chapter12/Calculator.java
@@ -0,0 +1,7 @@
+package chapter12;
+
+public class Calculator {
+	public int square(int x) {
+		return x * x;
+	}
+}
---
+++ src/chapter12/CalculatorTest.java
@@ -0,0 +1,19 @@
+package chapter12;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	@DisplayName("2の二乗は4になる")
+	void test() {
+		Calculator calc = new Calculator();
+		int result = calc.square(2);
+		assertThat(result, is(4));
+	}
+
+}
