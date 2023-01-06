ce7662f89826e7904279e514db8f59a9cdc448c0
vsckawa <109258421+hkreds@users.noreply.github.com>
2023-01-06 11:31:14 +0900

    [Finish]Chapter12 共通処理をまとめる方法

--- src/chapter12/CalculatorTest.java
+++ src/chapter12/CalculatorTest.java
@@ -1,30 +1,34 @@
 package chapter12;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	private Calculator calc;
+	@BeforeEach
+	void setUp() {
+		calc = new Calculator();
+	}
 	@Test
 	@DisplayName("2の二乗は4になる")
 	void testSquare2() {
-		Calculator calc = new Calculator();
 		int result = calc.square(2);
 		assertThat(result, is(4));
 	}
 	@Test
 	@DisplayName("4の平方根は2になる")
 	void testRoot4() {
-		Calculator calc = new Calculator();
 		double result = calc.root(4.0);
 		assertThat(result, is(2.0));
 	}
 	@Test
 	@DisplayName("-1の平方根は例外が発生する")
 	void testRootException() {
-		Calculator calc = new Calculator();
 		assertThrows(IllegalArgumentException.class, () -> calc.root(-1));
 	}
 }
